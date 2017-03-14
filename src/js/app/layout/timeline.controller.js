0bd2317e7ed66ce03ef95f88505d145d3e58eb4b
Viktor Ölund <viktor.olund@gmail.com>
2017-03-14 23:39:59 +0100

    refactor(index, footer): Added font awesome and new styles to main indexfile and footer

--- src/js/app/layout/timeline.controller.js
+++ src/js/app/layout/timeline.controller.js
@@ -13,7 +13,6 @@
             {
                 side: 'left',
                 badgeClass: 'info',
-                badgeIconClass: 'glyphicon-check',
                 title: 'Utbildning Umeå Universitet <small class="text-muted">Högskoleingenjörsprogrammet i Elektronik och Datorteknik</small>',
                 when: '2011-2014',
                 contentHtml: 'Kurser som behandlat områden i webb som C#, PHP, MySQL, JavaScript, html, CSS och programspråk som C och Python'
@@ -34,7 +33,6 @@
                     eventConfig = {
                         side: 'left',
                         badgeClass: 'info',
-                        badgeIconClass: 'glyphicon-check',
                         title: undefined,
                         when: undefined,
                         contentHtml: undefined
